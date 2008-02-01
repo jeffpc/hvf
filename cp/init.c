ab7d07a33e3c810a0a2d8b20ff244b91ad3e7416
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2008-02-01 17:14:45 -0500

    cp: allocate storage for guest & assemble guest address space
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/init.c
+++ cp/init.c
@@ -1,10 +1,39 @@
 #include <directory.h>
 #include <sched.h>
 #include <errno.h>
+#include <page.h>
+#include <buddy.h>
+#include <slab.h>
+#include <dat.h>
+
+static int __alloc_guest_storage(struct user *user)
+{
+	u64 pages = user->storage_size >> PAGE_SHIFT;
+	struct page *p;
+
+	while (pages) {
+		p = alloc_pages(0, ZONE_NORMAL);
+		if (!p)
+			continue; /* FIXME: sleep? */
+
+		pages--;
+
+		dat_insert_page(&current->guest->as, (u64) page_to_addr(p),
+				pages << PAGE_SHIFT);
+	}
+
+	return 0;
+}
 
 static int cp_init(void *data)
 {
 	struct user *user = data;
+
+	current->guest = malloc(sizeof(struct guest_state), ZONE_NORMAL);
+	BUG_ON(!current->guest);
+	memset(current->guest, 0, sizeof(struct guest_state));
+
+	__alloc_guest_storage(user);
 
 	for (;;) {
 	}
