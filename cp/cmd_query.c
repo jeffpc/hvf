80a1aea85e41397481bad95a06ee727f44737ef4
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2008-08-21 22:06:03 -0400

    cp: Implement QUERY TIME
    Much like the z/VM QUERY TIME, except that the connect/virtual/total cpu
    times are not printed (or even accounted).
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/cmd_query.c
+++ cp/cmd_query.c
@@ -5,6 +5,15 @@ static int cmd_query(struct user *u, char *cmd, int len)
 		con_printf(u->con, "IPL at %02d:%02d:%02d UTC %04d-%02d-%02d\n",
 			   ipltime.th, ipltime.tm, ipltime.ts, ipltime.dy,
 			   ipltime.dm, ipltime.dd);
+
+	} else if (!strcasecmp(cmd, "TIME")) {
+		struct datetime dt;
+
+		get_parsed_tod(&dt);
+
+		con_printf(u->con, "TIME IS %02d:%02d:%02d UTC %04d-%02d-%02d\n",
+			   dt.th, dt.tm, dt.ts, dt.dy, dt.dm, dt.dd);
+
 	} else
 		con_printf(u->con, "QUERY: Unknown variable '%s'\n", cmd);
 
