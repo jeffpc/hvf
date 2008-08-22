7fac482d2f1871e1aea2bd861f2484fc488a65f1
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2008-08-21 22:03:03 -0400

    cp: Implement QUERY CPLEVEL
    Much like the z/VM QUERY CPLEVEL, except that generation date/time is not
    printed (or saved anywhere in the nucleus).
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/cmd_query.c
+++ cp/cmd_query.c
@@ -1,5 +1,12 @@
 static int cmd_query(struct user *u, char *cmd, int len)
 {
-	con_printf(u->con, "got a query! '%s'\n", cmd);
+	if (!strcasecmp(cmd, "CPLEVEL")) {
+		con_printf(u->con, "HVF version " VERSION "\n");
+		con_printf(u->con, "IPL at %02d:%02d:%02d UTC %04d-%02d-%02d\n",
+			   ipltime.th, ipltime.tm, ipltime.ts, ipltime.dy,
+			   ipltime.dm, ipltime.dd);
+	} else
+		con_printf(u->con, "QUERY: Unknown variable '%s'\n", cmd);
+
 	return 0;
 }
