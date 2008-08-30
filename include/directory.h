b18242b8c8f1e379e22873aad8dc8f923828263e
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2008-08-30 14:31:51 -0400

    directory: virtual device struct definitions
    Unless I forgot something, there are 5 distinct device types:
    
    	VDEV_CONS:	The virtual device is a console
    	VDEV_DED:	The virtual device is backed by a dedicated real device
    	VDEV_SPOOL:	The virtual device is backed by the spool
    	VDEV_MDISK:	The virtual device is a minidisk
    	VDEV_LINK:	The virtual device is a link another user's vdev
    
    This patch sets up these 5 types, and not much more. Once more drivers are
    supported, the structures will grow more.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- include/directory.h
+++ include/directory.h
@@ -3,12 +3,54 @@
 
 #include <console.h>
 
+enum vdevtype {
+	VDEV_INVAL = 0,			/* invalid */
+	VDEV_CONS,			/* a console */
+	VDEV_DED,			/* dedicated real device */
+	VDEV_SPOOL,			/* backed by the spool */
+	VDEV_MDISK,			/* a minidisk */
+	VDEV_LINK,			/* a link to another user's device */
+};
+
+struct vdev {
+	enum vdevtype type;		/* device type */
+	u16 vdev;			/* virtual dev # */
+	u32 vsch;			/* virtual sch # */
+
+	union {
+		/* VDEV_CONS */
+		struct {
+		} console;
+
+		/* VDEV_DED */
+		struct {
+		} dedicate;
+
+		/* VDEV_SPOOL */
+		struct {
+			u16 type;
+			u8 model;
+		} spool;
+
+		/* VDEV_MDISK */
+		struct {
+		} mdisk;
+
+		/* VDEV_LINK */
+		struct {
+		} link;
+	} u;
+};
+
 struct user {
 	char *userid;
 	struct console *con;
 
 	/* VM configuration */
 	u64 storage_size;
+
+	/* FIXME: make this dynamically allocated */
+	struct vdev devices[5];
 };
 
 extern struct user *find_user_by_id(char *userid);
