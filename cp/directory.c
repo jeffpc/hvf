717c51bc9f3f8629e958e1dc00b81e5646d373c5
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2008-08-30 14:33:33 -0400

    cp: define 4 virtual device for the operator user
    This commit sets up a virtual CON, RDR, PUN, and PRT devices. Note that the
    device structures are allocated statically instead of the more logical
    dynamic allocation. This will change eventually.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/directory.c
+++ cp/directory.c
@@ -6,6 +6,39 @@ static struct user directory[] = {
 		.userid = "operator",
 		.con = NULL,
 		.storage_size = 2 * 1024 * 1024,
+		.devices = {
+			{
+				/* CON */
+				.type = VDEV_CONS,
+				.vdev = 0x0009,
+				.vsch = 0x10000,
+			},
+			{
+				/* RDR */
+				.type = VDEV_SPOOL,
+				.vdev = 0x000C,
+				.vsch = 0x10001,
+				.u.spool = { .type = 0x3505, .model = 1, },
+			},
+			{
+				/* PUN */
+				.type = VDEV_SPOOL,
+				.vdev = 0x000D,
+				.vsch = 0x10002,
+				.u.spool = { .type = 0x3525, .model = 1, },
+			},
+			{
+				/* PRT */
+				.type = VDEV_SPOOL,
+				.vdev = 0x000E,
+				.vsch = 0x10003,
+				.u.spool = { .type = 0x1403, .model = 1, },
+			},
+			{
+				/* END */
+				.type = VDEV_INVAL,
+			},
+		},
 	},
 
 	/* a NULL record to mark the end of the directory */
