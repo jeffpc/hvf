b553a14335cc3b374b9b87b9c7adf55b75ff981c
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-01-04 03:43:05 -0500

    cp: STORE STORAGE command
    Usage: STORE STORAGE <word> <address>
    
    Both <word> and <address> are in hex.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/cmd_store.c
+++ cp/cmd_store.c
@@ -1,3 +1,69 @@
+static int cmd_store_storage(struct user *u, char *cmd, int len)
+{
+	int ret;
+	u64 guest_addr, host_addr;
+	u32 val = 0, tmp=0;
+	int ws_only;
+
+	/* get the value */
+	for (ws_only=1; cmd && *cmd != '\0' && (*cmd != ' ' || ws_only); cmd++) {
+		if (*cmd >= '0' && *cmd <= '9')
+			tmp = *cmd - '0';
+		else if (*cmd >= 'A' && *cmd <= 'F')
+			tmp = *cmd - 'A' + 10;
+		else if (*cmd >= 'a' && *cmd <= 'f')
+			tmp = *cmd - 'a' + 10;
+		else if (*cmd == ' ' || *cmd == '\t') {
+			continue;
+		}
+
+		val = val*16 + tmp;
+		ws_only = 0;
+	}
+
+	if (ws_only)
+		return -EINVAL;
+
+	/* consume any extra whitespace */
+	while(*cmd == ' ' || *cmd == '\t')
+		cmd++;
+
+	ret = parse_addrspec(&guest_addr, cmd, len);
+	if (ret) {
+		con_printf(u->con, "DISPLAY: Invalid addr-spec '%s'\n", cmd);
+		return ret;
+	}
+
+	/*
+	 * round down to the nearest word
+	 */
+	guest_addr &= ~((u64) 3ULL);
+
+	con_printf(u->con, "guest:  addr = %llx\n", guest_addr);
+
+	/* walk the page tables to find the real page frame */
+	ret = virt2phy(&current->guest->as, guest_addr, &host_addr);
+	if (ret) {
+		con_printf(u->con, "DISPLAY: Specified address is not part of "
+			   "guest configuration\n");
+		return ret;
+	}
+
+	con_printf(u->con, "host:   addr = %llx\n", host_addr);
+
+	/*
+	 * this is a clever trick to have one format string and have it
+	 * print (almost arbitrary amount of data); we give it a length (in
+	 * digits) of the result, and always give it a u64 value
+	 */
+	con_printf(u->con, "R%016llX  will be set to %08lX\n", guest_addr,
+		   (unsigned long) val);
+
+	*((u32*) host_addr) = val;
+
+	return 0;
+}
+
 static int cmd_store_psw(struct user *u, char *cmd, int len)
 {
 	u32 *ptr = (u32*) &current->guest->sie_cb.gpsw;
@@ -55,8 +121,8 @@ static int cmd_store_psw(struct user *u, char *cmd, int len)
 }
 
 static struct cpcmd cmd_tbl_store[] = {
-#if 0
 	{"STORAGE", cmd_store_storage, NULL},
+#if 0
 	{"GPR", cmd_store_gpr, NULL},
 	{"FPR", cmd_store_fpr, NULL},
 	{"CR", cmd_store_cr, NULL},
