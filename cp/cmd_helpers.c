4a7754aaadaa902695ad79b0a5172d66704fdafa
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-01-05 20:04:44 -0500

    cp: helper function to parse decimal numbers
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/cmd_helpers.c
+++ cp/cmd_helpers.c
@@ -1,3 +1,34 @@
+static char* __extract_dec(char *str, u64 *val)
+{
+	u64 res;
+	u64 tmp;
+	int len;
+
+	if (!str || !val)
+		return ERR_PTR(-EINVAL);
+
+	res = 0;
+	len = -1;
+
+	for (; str && *str != '\0'; str++, len++) {
+		if (*str >= '0' && *str <= '9')
+			tmp = *str - '0';
+		else if (*str == ' ' || *str == '\t')
+			break;
+		else
+			return ERR_PTR(-EINVAL);
+
+		res = (res * 10) + tmp;
+	}
+
+	if (len == -1)
+		return ERR_PTR(-EINVAL);
+
+	*val = res;
+
+	return str;
+}
+
 static char* __extract_hex(char *str, u64 *val)
 {
 	u64 res;
