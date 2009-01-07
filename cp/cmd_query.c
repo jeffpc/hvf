77956bbcd11dd128f73b3da48a3917ce2becb9b4
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-01-07 02:22:11 -0500

    cp: move bulk of QUERY VIRTUAL into a separate function
    This saves us 2 levels of indentation in the most annoying part of the
    command.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/cmd_query.c
+++ cp/cmd_query.c
@@ -10,6 +10,45 @@ static char *__guest_state_to_str(enum virt_cpustate st)
 	return "???";
 }
 
+static void display_vdev(struct console *con, struct vdev *vdev)
+{
+	switch (vdev->type) {
+		case VDEV_CONS:
+			con_printf(con, "CONS %04X 3215 ON %s %04X SCH = %05X\n",
+				   vdev->vdev, type2name(con->dev->type),
+				   con->dev->ccuu, vdev->vsch);
+			break;
+		case VDEV_DED:
+			con_printf(con, "???? %04X SCH = %05X\n",
+				   vdev->vdev, vdev->vsch);
+			break;
+		case VDEV_SPOOL:
+			con_printf(con, "%-4s %04X %04X SCH = %05X\n",
+				   type2name(vdev->u.spool.type),
+				   vdev->vdev, vdev->u.spool.type,
+				   vdev->vsch);
+			break;
+		case VDEV_MDISK:
+			con_printf(con, "DASD %04X 3390 %6d CYL ON DASD %04X SCH = %05X\n",
+				   vdev->vdev,
+				   vdev->u.mdisk.cylcnt, /* cyl count */
+				   vdev->u.mdisk.rdev,   /* rdev */
+				   vdev->vsch);
+			break;
+		case VDEV_LINK:
+			con_printf(con, "LINK %04X TO %s %04X SCH = %05X\n",
+				   vdev->vdev,
+				   "????", /* userid */
+				   0xffff, /* user's vdev # */
+				   vdev->vsch);
+			break;
+		default:
+			con_printf(con, "???? unknown device type (%04X, %05X)\n",
+				   vdev->vdev, vdev->vsch);
+			break;
+	}
+}
+
 static int cmd_query(struct virt_sys *sys, char *cmd, int len)
 {
 	if (!strcasecmp(cmd, "CPLEVEL")) {
@@ -27,51 +66,13 @@ static int cmd_query(struct virt_sys *sys, char *cmd, int len)
 			   dt.th, dt.tm, dt.ts, dt.dy, dt.dm, dt.dd);
 
 	} else if (!strcasecmp(cmd, "VIRTUAL")) {
-		struct vdev *vdev;
 		int i;
 
 		con_printf(sys->con, "CPU %s\n", __guest_state_to_str(sys->task->cpu->state));
 		con_printf(sys->con, "STORAGE = %lluM\n", sys->directory->storage_size >> 20);
 
-		for(i=0; sys->directory->devices[i].type != VDEV_INVAL; i++) {
-			vdev = &sys->directory->devices[i];
-
-			switch (vdev->type) {
-				case VDEV_CONS:
-					con_printf(sys->con, "CONS %04x 3215 ON %s %04x SCH = %05x\n",
-						   vdev->vdev, type2name(sys->con->dev->type),
-						   sys->con->dev->ccuu, vdev->vsch);
-					break;
-				case VDEV_DED:
-					con_printf(sys->con, "???? %04x SCH = %05x\n",
-						   vdev->vdev, vdev->vsch);
-					break;
-				case VDEV_SPOOL:
-					con_printf(sys->con, "%-4s %04x %04x SCH = %05x\n",
-						   type2name(vdev->u.spool.type),
-						   vdev->vdev, vdev->u.spool.type,
-						   vdev->vsch);
-					break;
-				case VDEV_MDISK:
-					con_printf(sys->con, "DASD %04x 3390 %6d CYL ON DASD %04x SCH = %05x\n",
-						   vdev->vdev,
-						   vdev->u.mdisk.cylcnt,/* cyl count */
-						   vdev->u.mdisk.rdev,	/* rdev */
-						   vdev->vsch);
-					break;
-				case VDEV_LINK:
-					con_printf(sys->con, "LINK %04x TO %s %04x SCH = %05x\n",
-						   vdev->vdev,
-						   "????", /* userid */
-						   0xffff, /* user's vdev # */
-						   vdev->vsch);
-					break;
-				default:
-					con_printf(sys->con, "???? unknown device type (%04x, %05x)\n",
-						   vdev->vdev, vdev->vsch);
-					break;
-			}
-		}
+		for(i=0; sys->directory->devices[i].type != VDEV_INVAL; i++)
+			display_vdev(sys->con, &sys->directory->devices[i]);
 
 	} else
 		con_printf(sys->con, "QUERY: Unknown variable '%s'\n", cmd);
