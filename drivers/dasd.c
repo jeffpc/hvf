823ae1bad1539ff718ac27b09aec3c26cb04de1f
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-07-28 04:27:35 -0400

    dasd: rename the 3390 functions & structs to contain '3390'
    3390 aren't the only DASDs worth supporting. There are 9336s which are FBA.
    This change allows multiple DASD drivers to easily co-exist.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- drivers/dasd.c
+++ drivers/dasd.c
@@ -2,7 +2,7 @@
 #include <console.h>
 #include <list.h>
 
-static int dasd_snprintf(struct device *dev, char* buf, int len)
+static int d3390_snprintf(struct device *dev, char* buf, int len)
 {
 	int cyls = 0;
 
@@ -14,7 +14,7 @@ static int dasd_snprintf(struct device *dev, char* buf, int len)
 	return snprintf(buf, len, "%10d CYL ", cyls);
 }
 
-static int dasd_reg(struct device *dev)
+static int d3390_reg(struct device *dev)
 {
 	switch(dev->model) {
 		case 0x0c: return 0; /* 3390-3 */
@@ -24,17 +24,17 @@ static int dasd_reg(struct device *dev)
 	return -ENOENT;
 }
 
-static struct device_type ddasd = {
-	.types		= LIST_HEAD_INIT(ddasd.types),
-	.reg		= dasd_reg,
+static struct device_type d3390 = {
+	.types		= LIST_HEAD_INIT(d3390.types),
+	.reg		= d3390_reg,
 	.interrupt	= NULL,
-	.snprintf	= dasd_snprintf,
+	.snprintf	= d3390_snprintf,
 	.type		= 0x3390,
 	.all_models	= 1,
 };
 
 int register_driver_dasd(void)
 {
-	return register_device_type(&ddasd);
+	return register_device_type(&d3390);
 }
 
