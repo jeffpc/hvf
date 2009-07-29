e50458acced856b6a3ba2b4502cfb43c939b67b6
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-07-29 18:42:58 -0400

    cp: STSCH handler
    With the exception of page-boundary crossing, it should be all done.
    
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/instruction_priv.c
+++ cp/instruction_priv.c
@@ -1,5 +1,8 @@
+#include <list.h>
 #include <sched.h>
 #include <cp.h>
+#include <vdevice.h>
+#include <vcpu.h>
 
 static int handle_msch(struct virt_sys *sys)
 {
@@ -15,8 +18,68 @@ static int handle_ssch(struct virt_sys *sys)
 
 static int handle_stsch(struct virt_sys *sys)
 {
-	con_printf(sys->con, "STSCH handler\n");
-	return -ENOMEM;
+	struct virt_cpu *cpu = sys->task->cpu;
+
+	u64 r1   = __guest_gpr(cpu, 1);
+	u64 addr = RAW_S_1(cpu);
+
+	struct schib *gschib;
+	struct virt_device *vdev;
+	int ret = 0;
+
+	if ((PAGE_SIZE-(addr & PAGE_MASK)) < sizeof(struct schib)) {
+		con_printf(sys->con, "The SCHIB crosses page boundary (%016llx; %lu)! CPU stopped\n",
+			   addr, sizeof(struct schib));
+		cpu->state = GUEST_STOPPED;
+		goto out;
+	}
+
+	/* sch number must be: X'0001____' */
+	if ((r1 & 0xffff0000) != 0x00010000) {
+		queue_prog_exception(sys, PROG_OPERAND, r1);
+		goto out;
+	}
+
+	/* schib must be word-aligned */
+	if (addr & 0x3) {
+		queue_prog_exception(sys, PROG_SPEC, addr);
+		goto out;
+	}
+
+	/* find the virtual device */
+	list_for_each_entry(vdev, &sys->virt_devs, devices) {
+		if (vdev->sch == (u32) r1)
+			break;
+	}
+
+	/* There's no virtual device with this sch number; CC=3 */
+	if (vdev == container_of(&sys->virt_devs, struct virt_device, devices)) {
+		cpu->sie_cb.gpsw.cc = 3;
+		goto out;
+	}
+
+	/* translate guest address to host address */
+	ret = virt2phy_current(addr, &addr);
+	if (ret) {
+		if (ret == -EFAULT) {
+			ret = 0;
+			queue_prog_exception(sys, PROG_ADDR, addr);
+		}
+
+		goto out;
+	}
+
+	gschib = (struct schib*) addr;
+
+	/* copy! */
+	memcpy(&gschib->pmcw, &vdev->pmcw, sizeof(struct pmcw));
+	memcpy(&gschib->scsw, &vdev->scsw, sizeof(struct scsw));
+
+	/* CC: 0 */
+	cpu->sie_cb.gpsw.cc = 0;
+
+out:
+	return ret;
 }
 
 static const intercept_handler_t instruction_priv_funcs[256] = {
