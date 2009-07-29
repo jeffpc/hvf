da4136292e1b8f389cdfdd02559d0c556cd04b85
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-07-29 18:41:13 -0400

    vcpu: register reading & address calculation helper functions
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- include/vcpu.h
+++ include/vcpu.h
@@ -18,4 +18,35 @@ enum PROG_EXCEPTION {
 
 extern void queue_prog_exception(struct virt_sys *sys, enum PROG_EXCEPTION type, u64 param);
 
+/*****************************************************************************/
+/* Guest register reading & address calculation                              */
+
+static inline u64 __guest_gpr(struct virt_cpu *cpu, int gpr)
+{
+	u64 ret = cpu->regs.gpr[gpr];
+
+	if (!(atomic_read(&cpu->sie_cb.cpuflags) & CPUSTAT_ZARCH))
+		ret &= 0xffffffffULL;
+
+	return ret;
+}
+
+static inline u64 __guest_addr(struct virt_cpu *cpu, u64 disp, int x, int b)
+{
+	u64 mask;
+
+	if (cpu->sie_cb.gpsw.ea && cpu->sie_cb.gpsw.ba)
+		mask = ~0;
+	else if (!cpu->sie_cb.gpsw.ea && cpu->sie_cb.gpsw.ba)
+		mask = 0x7fffffff;
+	else if (!cpu->sie_cb.gpsw.ea && !cpu->sie_cb.gpsw.ba)
+		mask = 0x00ffffff;
+	else
+		BUG();
+
+	return (disp +
+		(x ? __guest_gpr(cpu, x) : 0) +
+		(b ? __guest_gpr(cpu, b) : 0)) & mask;
+}
+
 #endif
