27ce7927371aa332a130647eaed92a0b05476b85
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2009-09-04 20:46:32 -0400

    types: create a top-level include dir with common headers
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- sys/include/loader.h
+++ sys/include/loader.h
@@ -3,18 +3,6 @@
 
 #define TEMP_BASE	((unsigned char*) 0x400000) /* 4MB */
 
-typedef unsigned long u64;
-typedef signed long s64;
-
-typedef unsigned int u32;
-typedef signed int s32;
-
-typedef unsigned short u16;
-typedef signed short s16;
-
-typedef unsigned char u8;
-typedef signed char s8;
-
 extern unsigned char ORB[32];
 
 #define memcpy(d,s,l)	__builtin_memcpy((d), (s), (l))
