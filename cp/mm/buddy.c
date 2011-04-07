425c703f16972b5af271e5d418d2e67b17e0ebba
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2011-04-07 12:19:14 -0400

    cp/mm: fix warning in buddy allocation init code
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- cp/mm/buddy.c
+++ cp/mm/buddy.c
@@ -29,7 +29,7 @@ static void __init_buddy_alloc(u64 base, int pages, int max_order,
 		INIT_LIST_HEAD(&orders[order]);
 
 		if (pages & (1 << order)) {
-			p = addr_to_page(base);
+			p = addr_to_page((void*) base);
 
 			list_add(&p->buddy, &orders[order]);
 
