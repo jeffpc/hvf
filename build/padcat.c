96fed13653056be3989e05759947aab657a5d814
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2011-04-11 23:00:13 -0400

    build: added a simple cat-like program tha pads each file to a multiple of 80 bytes
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

---
+++ build/padcat.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+
+void cat(char *fn)
+{
+	FILE *f;
+	char buf[80];
+	size_t n;
+
+	f = fopen(fn, "rb");
+	if (!f) {
+		fprintf(stderr, "Could not open file \"%s\". Skipping.\n",
+			fn);
+		return;
+	}
+
+	do {
+		memset(buf, 0, 80);
+		n = fread(buf, 1, 80, f);
+		if (n)
+			assert(fwrite(buf, 1, 80, stdout) == 80);
+	} while(n == 80);
+
+	fclose(f);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for(i=1; i<argc; i++)
+		cat(argv[i]);
+
+	return 0;
+}
