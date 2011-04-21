8e4d7aa3cbe3ec127801fc8b6b34fc14f83fd3e0
Josef 'Jeff' Sipek <jeffpc@josefsipek.net>
2011-04-21 13:22:05 -0400

    installer: actually install the CP binary
    Signed-off-by: Josef 'Jeff' Sipek <jeffpc@josefsipek.net>

--- installer/cpio.c
+++ installer/cpio.c
@@ -32,7 +32,7 @@ struct table {
 static struct table table[] = {
 	{"hvf.directory",	"HVF     ", "DIRECT  ", 80,   1, 0},
 	{"hvf.config",		"SYSTEM  ", "CONFIG  ", 80,   1, 0},
-//	{"hvf",			"HVF     ", "ELF     ", 4096, 0, 0},
+	{"hvf",			"HVF     ", "ELF     ", 4096, 0, 0},
 	{"eckd.rto",		"ECKDLOAD", "BIN     ", 4096, 0, 1},
 	{"loader.rto",		"DASDLOAD", "BIN     ", 4096, 0, 2},
 	{"installed_files.txt",	"HVF     ", "TEXT    ", 80,   1, 0},
